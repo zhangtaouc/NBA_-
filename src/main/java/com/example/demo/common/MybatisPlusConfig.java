e110a307578344085f07e0603fe3b9ad7b298386
zhangtao <3131966940@qq.com>
2022-01-16 13:46:10 +0800

    初次提交demo

---
+++ src/main/java/com/example/demo/common/MybatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.common;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.mybatis.spring.annotation.MapperScan;
+@Configuration
+@MapperScan("com.example.demo.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
